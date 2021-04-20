96651a383dfd273325f6d2551c2c86af9dcd3fdd
LaibaNasir-1 <82863816+LaibaNasir-1@users.noreply.github.com>
2021-04-20 11:22:46 +0500

    Create Task2-InfuraURLWeb.js

---
+++ Task2-InfuraURLWeb.js
@@ -0,0 +1,21 @@
+//const Web3 = require('web3'); //Now we have access to a variable where you can create a new Web3 connection
+//console.log(Web3)
+//Commented here because importing in html file
+
+const rpcUrl = "https://ropsten.infura.io/v3/9d3abb594bbb4be99e86186d3d9e87de";
+let web3 = new Web3(rpcUrl); 
+console.log("rpc", web3) 
+
+let address = "0x8C91F839D66C21fA5E1edFE603aB8A69cEC8324A";
+web3.eth.getBalance(address, (err, wei)=> { //this arrow function is from ECMA6 we can use normal function keyword here as well
+    if(err){
+        console.log("This is an error", err)
+    }
+    else{
+        console.log('wei balance', wei) //showing balances in wei of the address
+        document.getElementById("main").innerHTML = `Balance in wei: ${wei}`
+        let balance = web3.utils.fromWei(wei, "ether");
+        console.log("balance in ethers", balance);
+        document.getElementById("mains").innerHTML = `Balance in ethers: ${balance}`
+    }
+})
